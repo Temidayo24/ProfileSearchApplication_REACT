2c568fca6bedbe2504c3854e0cf2160511603efb
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-04-10 18:41:35 +0100

    Uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile from "./Profile";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [filteredProfile, setFilteredProfile] = new useState(profile);
+
+  const profileLists = filteredProfile.map((person) => (
+    <li key={person.id} className="profile">
+      <img src={person.picture} alt={person.firstName + "'s picture"} />
+      <div className="personProfile">
+        <span className="personID">{person.id}</span>
+        <span className="personName">
+          {person.title + " " + person.firstName + " " + person.lastName}
+        </span>
+      </div>
+    </li>
+  ));
+
+  const filterBySearch = (e) => {
+    const query = e.target.value;
+    let updatedList = [...profile];
+    updatedList = updatedList.filter((person) => {
+      return (
+        person.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        person.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        person.id.toLowerCase().indexOf(query.toLowerCase()) !== -1 ||
+        person.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+      );
+    });
+
+    setFilteredProfile(updatedList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form className="profileSearchBar">
+        <input placeholder="Search by name..." onChange={filterBySearch} />
+      </form>
+
+      <ul className="profileContainer">{profileLists}</ul>
     </div>
   );
-}
+};
 
 export default App;
